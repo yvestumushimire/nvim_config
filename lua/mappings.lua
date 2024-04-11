5ed0acdf687cb8255fbc6609b9751c3bc4c3cf9f
Yves Tumushimire <yvestumushimire@gmail.com>
2024-04-11 16:10:10 +0200

    important mappings
    Signed-off-by: Yves Tumushimire <yvestumushimire@gmail.com>

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -11,3 +11,10 @@ map("i", "jk", "<ESC>")
 map("n", "<leader>db", function() require("dap").toggle_breakpoint() end)
 
 map("n", "<leader>dpr", function() require("dap-python").test_method() end)
+
+map("n", "<A-j>", "<cmd>m .+1<CR>==", {desc="Move line down"})
+map("n", "<A-k>", "<cmd>m .-2<CR>==", {desc="Move line up"})
+map("i", "<A-j>", "<ESC><cmd>m .+1<CR>==gi", {desc="Move line down"})
+map("i", "<A-k>", "<ESC><cmd>m .-2<CR>==gi", {desc="Move line up"})
+map("v", "J", ":m '>+1<CR>gv=gv", {desc="Move line down"})
+map("v", "K", ":m '<-2<CR>gv=gv", {desc="Move line up"})
